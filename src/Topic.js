38537241f9c6be1373a6cc4f59375b21bf54c3f3
Olim Gulomov <113071359+olimgulomov98@users.noreply.github.com>
2022-09-25 12:26:54 +0400

    about github

---
+++ src/Topic.js
@@ -0,0 +1,31 @@
+// <!-- Git hub -->
+// <!-- 
+//     -> birinchi projectni githubga yuklash
+//     git init 
+//     git add . 
+//     git commit -m 'commentariya'
+//     git remote add origin https://github.com/username/dsfsd.git
+//     git push -u origin master
+
+//     -> qoshimcha kiritilganda githubga qoshib qoyish
+//     git add .
+//     git commit -m 'navbar'
+//     git push origin master (git push)
+
+//     -> branch ochish
+//     git branch 
+//     git branch navbar -> navbar branchini ochish
+//     git checkout navbar -> usha branchga otib olamiz
+
+//     -> kodlarni shu branchda yozib bolib saxranit qilamimz va
+//     git add .
+//     git commit -m 'navbar tugatildi'
+//     git push origin navbar
+
+//     o'zgarishlarni tortib olish uchun 
+//     git pull origin master
+
+//     git config --global user.email 'email.com'
+//     git config --global user.name 'username'
+
+//  -->
