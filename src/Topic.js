cee3a69c9b02f604ab3317c93481171ed8630756
Olim Gulomov <113071359+olimgulomov98@users.noreply.github.com>
2022-09-25 12:29:48 +0400

    how to create react file

---
+++ src/Topic.js
@@ -0,0 +1,5 @@
+// Babel -> reactda yozilgan kodni browser o'qiydigan korinishga
+// o'tkazib berish uchun ishlatiladigan kutibxona
+// npx -> HTML+XML - htmlda yozilgan kodni js yozish imkoni beradi. 
+// js da yozilgan kodni html da ishlatish imkonini beradigan sintaksis
+// npx create-react-app . -> project create qilish
